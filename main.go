5a41756951f3bc8ebc042b5cea0b0b7eaf008777
Yulia Rovnik <32664910+bieddes-blade@users.noreply.github.com>
2024-11-15 22:41:25 -0500

    Update main.go

--- main.go
+++ main.go
@@ -11,10 +11,13 @@ import (
     "sync"
 )
 
-const MAX_THREADS = 400
+const MAX_THREADS = 100
 const MAX_IP = 256
+const BUF_SIZE = 1000
+
 var wg sync.WaitGroup
-var mutex sync.Mutex
+var channel [MAX_THREADS](chan string)
+var partNonUnique [MAX_THREADS]int
 var table [MAX_IP][MAX_IP][MAX_IP][MAX_IP / 8]uint8
 var lineCounter, nonUnique = 0, 0
 
@@ -33,27 +36,27 @@ func setBit(n uint8, pos uint8) uint8 {
     return n
 }
 
-func processLine(address string) {
+func threadLoop(id int, c chan string) {
     defer wg.Done()
 
-    parts := strings.Split(address, ".")
+    for address := range c {
+        parts := strings.Split(address, ".")
 
-    index := [4]int{}
-    for i := 0; i < 4; i += 1 {
-        value, _ := strconv.Atoi(parts[i])
-        index[i] = value
+        index := [4]int{}
+        for i := 0; i < 4; i += 1 {
+            value, _ := strconv.Atoi(parts[i])
+            index[i] = value
+        }
+
+        p := &table[index[0]][index[1]][index[2]][index[3] / 8]
+        shift := uint8(index[3] % 8)
+
+        if hasBit(*p, shift) {
+            partNonUnique[id] += 1
+        } else {
+            *p = setBit(*p, shift)
+        }
     }
-
-    mutex.Lock()
-    p := &table[index[0]][index[1]][index[2]][index[3] / 8]
-    shift := uint8(index[3] % 8)
-
-    if hasBit(*p, shift) {
-        nonUnique += 1
-    } else {
-        *p = setBit(*p, shift)
-    }
-    mutex.Unlock()
 }
 
 func main() {
@@ -66,30 +69,29 @@ func main() {
     }
     defer file.Close()
 
-    buffer := [MAX_THREADS]string{}
-    bufCounter := 0
+    wg.Add(MAX_THREADS)
+    for i := 0; i < MAX_THREADS; i += 1 {
+        channel[i] = make(chan string, BUF_SIZE)
+        go threadLoop(i, channel[i])
+    }
 
     scanner := bufio.NewScanner(file)
     for scanner.Scan() {
         lineCounter += 1
-        buffer[bufCounter] = scanner.Text()
-        bufCounter += 1
-        if bufCounter == MAX_THREADS {
-            wg.Add(MAX_THREADS)
-            for i := 0; i < MAX_THREADS; i += 1 {
-                go processLine(buffer[i])
-            }
-            wg.Wait()
-            bufCounter = 0
-        }
+        firstPart, _ := strings.CutPrefix(scanner.Text(), ".")
+        firstNum, _ := strconv.Atoi(firstPart)
+        id := firstNum / (MAX_IP / MAX_THREADS)
+        
+        channel[id] <- scanner.Text()
     }
 
-    if bufCounter != 0 {
-        wg.Add(bufCounter)
-        for i := 0; i < bufCounter; i += 1 {
-            go processLine(buffer[i])
-        }
-        wg.Wait()
+    for i := 0; i < MAX_THREADS; i += 1 {
+        close(channel[i])
+    }
+    wg.Wait()
+
+    for i := 0; i < MAX_THREADS; i += 1 {
+        nonUnique += partNonUnique[i];
     }
 
     fmt.Printf("All lines: %d, Unique lines: %d\n", lineCounter, lineCounter - nonUnique)
